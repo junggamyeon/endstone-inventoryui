0f42addb50228c9ad5aa73e5f2fe7306dfef088a
MakiTazo <laalberto96@gmail.com>
2026-08-16 11:10:46 -0600

    fix: correct item serialization format in ItemStackWrapper

--- src/jwinventoryapi/network/item_stack_wrapper.py
+++ src/jwinventoryapi/network/item_stack_wrapper.py
@@ -15,15 +15,6 @@ class ItemStackWrapper:
             data = get_item_data("minecraft:air")
         self.data: ItemData = data
 
-    def write_header(self, stream: BinaryStream) -> bool:
-        if is_air(self.item_stack):
-            stream.write_varint(0)
-            return False
-        stream.write_varint(self.data.item_id)
-        stream.write_unsigned_short(self.item_stack.amount)
-        stream.write_unsigned_varint(self.item_stack.data)
-        return True
-
     def write_footer(self, stream: BinaryStream):
         item_meta = self.item_stack.item_meta
         tag = build_tag(item_meta)
@@ -37,30 +28,22 @@ class ItemStackWrapper:
         stream.write_unsigned_int(0)
 
     def write(self, stream: BinaryStream):
-        if self.write_header(stream):
-            has_net_id = self.stack_id != 0
-            stream.write_bool(has_net_id)
-            if has_net_id:
-                stream.write_varint(self.stack_id)
-            stream.write_varint(0)
-            user_data = BinaryStream()
-            self.write_footer(user_data)
-            stream.write_bytes(user_data.copy_buffer())
-
-    def write_descriptor(self, stream: BinaryStream):
         is_air_item = is_air(self.item_stack)
         has_net_id = self.stack_id != 0
+
         stream.write_signed_short(0 if is_air_item else self.data.item_id)
         stream.write_unsigned_short(self.item_stack.amount)
         stream.write_unsigned_varint(self.item_stack.data)
+
         stream.write_bool(has_net_id)
         if has_net_id:
-            stream.write_unsigned_varint(0)
             stream.write_varint(self.stack_id)
+
         stream.write_unsigned_varint(0)
         if is_air_item:
             stream.write_unsigned_varint(0)
             return
+
         user_data = BinaryStream()
         self.write_footer(user_data)
         stream.write_bytes(user_data.copy_buffer())
